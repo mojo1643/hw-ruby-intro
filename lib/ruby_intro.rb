b12183465c3745d722de5c868e617e1a0d3f7dc8
EC2 Default User <ec2-user@ip-172-31-30-33.us-east-2.compute.internal>
2019-02-04 16:27:58 +0000

    ruby_intro_complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,85 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  raise ArgumentError, "Invalid Array Obj." if !arr.is_a?(Array)
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  raise ArgumentError, "Invalid Array Obj." if !arr.is_a?(Array)
+  
+  if arr.empty? 
+    return 0
+  else
+    arr.max(2).reduce(:+)
+  end
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  raise ArgumentError, "Invalid Array Obj." if !arr.is_a?(Array)
+  
+  num_arr = arr.combination(2).find{|x,y| x+y==n}
+  if num_arr == nil
+    return false
+  else
+    return true
+  end
+  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+   raise ArgumentError, "Invalid String." if !name.is_a?(String)
+   return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  raise ArgumentError, "Invalid String." if !s.is_a?(String)
+  /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  raise ArgumentError, "Invalid String." if !s.is_a?(String)
+  
+  # check for single string 0
+  if s.eql?('0')
+    return true
+  end
+  
+  /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
+  def initialize(isbn, price)
+    raise ArgumentError, "ISBN is empty." if isbn.empty?
+    raise ArgumentError, "Price should be greater than 0." if price<=0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn                   # creates getter method for isbn
+    @isbn
+  end
+  
+  def isbn=(_isbn)           # creates setter method for isbn
+    @isbn = _isbn
+  end
+  
+  def price                  # creates getter method for price
+    @price
+  end
+  
+  def price=(_price)         # creates setter method for price
+    @price = _price
+  end
+  
+  def price_as_string
+    return "$#{'%.2f' %price}"
+  end
+  
 # YOUR CODE HERE
 end
